5a8c050fccd354d81cd46d77c0ea4fd2991cb73f
gibbonzen <julian.mauborgne@gmail.com>
2018-07-19 14:16:15 +0200

    allow origin

--- server_k.js
+++ server_k.js
@@ -20,6 +20,12 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 global.config = Tools.loadJsonFile('./config_server_k.json')
 
 let connection = config.server
